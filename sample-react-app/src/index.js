4a6c17c177fde1e11a34e4b20d652e4cafafa382
Nishaat Rajabali <12186256+nishaatr@users.noreply.github.com>
2023-06-07 15:22:22 +0100

    Update index.js

--- sample-react-app/src/index.js
+++ sample-react-app/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode>// NISHAAT
     <App />
   </React.StrictMode>,
   document.getElementById('root')
